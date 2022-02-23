38a082208aa377c2bf2d817a87f2ff7504e7ebcf
Caio <caio8200@gmail.com>
2022-02-23 17:33:23 -0300

    test update last case

--- src/modules/customers/services/tests/UpdateCustomerService.spec.ts
+++ src/modules/customers/services/tests/UpdateCustomerService.spec.ts
@@ -2,15 +2,18 @@ import 'reflect-metadata';
 import UpdateCustomerService from '../UpdateCustomerService';
 import FakeCustomersRepository from '@modules/customers/domain/repositories/fakes/FakeCustomerRepository';
 import AppError from '@shared/errors/AppError';
+import CreateCustomerService from '../CreateCustomerService';
 
 
 let fakeCustomersRepository: FakeCustomersRepository;
-let updateCustomer: UpdateCustomerService; 
+let updateCustomer: UpdateCustomerService;
+let createCustomer: CreateCustomerService;
 
 describe('UpdateCustomer', () => {
     beforeEach(() => {
         fakeCustomersRepository = new FakeCustomersRepository();
         updateCustomer = new UpdateCustomerService(fakeCustomersRepository);
+        createCustomer = new CreateCustomerService(fakeCustomersRepository);
     });
 
     it('should be able to update a existent customer', async () => {
@@ -37,5 +40,16 @@ describe('UpdateCustomer', () => {
               email: 'caio@faria',
         }),
       ).rejects.toBeInstanceOf(AppError);
+    })
+
+    it('should not be able to update to a existent email', async () => {
+      
+      const updated = await updateCustomer.execute({
+        id: 'fake_uuid_v4',
+        email: "test_diff@gmail.com",
+        name: 'caio',
+      })
+
+      expect(updated).rejects.toBeInstanceOf(AppError);
     })
 })
